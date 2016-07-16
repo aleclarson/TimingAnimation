acc0ca74c522f552ea044ab121b46958c3cb40bd
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-07-15 18:30:37 -0700

    1.0.0

---
+++ js/TimingAnimation.js
@@ -0,0 +1,121 @@
+var Animation, Easing, LazyVar, Type, fromArgs, type;
+
+Animation = require("Animated").Animation;
+
+fromArgs = require("fromArgs");
+
+LazyVar = require("LazyVar");
+
+Easing = require("easing");
+
+Type = require("Type");
+
+type = Type("TimingAnimation");
+
+type.inherits(Animation);
+
+type.optionTypes = {
+  endValue: Number,
+  duration: Number,
+  easing: Function,
+  delay: Number
+};
+
+type.optionDefaults = {
+  easing: Easing("linear"),
+  delay: 0
+};
+
+type.defineFrozenValues({
+  endValue: fromArgs("endValue"),
+  duration: fromArgs("duration"),
+  easing: fromArgs("easing"),
+  delay: fromArgs("delay"),
+  _velocity: function() {
+    return LazyVar((function(_this) {
+      return function() {
+        return (_this.value - _this._lastValue) / (_this.time - _this._lastTime);
+      };
+    })(this));
+  }
+});
+
+type.defineValues({
+  progress: 0,
+  time: null,
+  value: null,
+  _timer: null,
+  _lastTime: null,
+  _lastValue: null
+});
+
+type.exposeLazyGetters(["velocity"]);
+
+type.defineMethods({
+  computeValueAtProgress: function(progress) {
+    return this.startValue + progress * (this.endValue - this.startValue);
+  },
+  computeProgressAtTime: function(time) {
+    if (time <= 0) {
+      return this.easing(0);
+    }
+    if (time >= this.duration) {
+      return this.easing(1);
+    }
+    return this.easing(time / this.duration);
+  },
+  _start: function() {
+    this._timer = null;
+    if (this.duration === 0) {
+      this._onUpdate(this.computeValueAtProgress(1));
+      this.finish();
+      return;
+    }
+    this.startTime = Date.now();
+    return this._requestAnimationFrame();
+  }
+});
+
+type.overrideMethods({
+  __computeValue: function() {
+    this._lastTime = this.time;
+    this._lastValue = this.value;
+    this._velocity.reset();
+    this.time = Math.min(this.duration, Date.now() - this.startTime);
+    this.progress = this.computeProgressAtTime(this.time);
+    return this.value = this.computeValueAtProgress(this.progress);
+  },
+  __didStart: function() {
+    if (this.delay <= 0) {
+      return this._start();
+    }
+    return this._timer = Timer(this.delay, (function(_this) {
+      return function() {
+        return _this._start();
+      };
+    })(this));
+  },
+  __didUpdate: function(value) {
+    if (this.time === this.duration) {
+      return this.finish();
+    }
+  },
+  __didEnd: function() {
+    if (!this._timer) {
+      return;
+    }
+    this._timer.prevent();
+    return this._timer = null;
+  },
+  __captureFrame: function() {
+    return {
+      progress: this.progress,
+      value: this.value,
+      time: this.time
+    };
+  }
+});
+
+module.exports = type.build();
+
+//# sourceMappingURL=map/TimingAnimation.map
---
+++ src/TimingAnimation.coffee
@@ -0,0 +1,102 @@
+
+{ Animation } = require "Animated"
+
+fromArgs = require "fromArgs"
+LazyVar = require "LazyVar"
+Easing = require "easing"
+Type = require "Type"
+
+type = Type "TimingAnimation"
+
+type.inherits Animation
+
+type.optionTypes =
+  endValue: Number
+  duration: Number
+  easing: Function
+  delay: Number
+
+type.optionDefaults =
+  easing: Easing "linear"
+  delay: 0
+
+type.defineFrozenValues
+
+  endValue: fromArgs "endValue"
+
+  duration: fromArgs "duration"
+
+  easing: fromArgs "easing"
+
+  delay: fromArgs "delay"
+
+  _velocity: -> LazyVar =>
+    (@value - @_lastValue) / (@time - @_lastTime)
+
+type.defineValues
+
+  progress: 0
+
+  time: null
+
+  value: null
+
+  _timer: null
+
+  _lastTime: null
+
+  _lastValue: null
+
+type.exposeLazyGetters [
+  "velocity"
+]
+
+type.defineMethods
+
+  computeValueAtProgress: (progress) ->
+    @startValue + progress * (@endValue - @startValue)
+
+  computeProgressAtTime: (time) ->
+    return @easing 0 if time <= 0
+    return @easing 1 if time >= @duration
+    return @easing time / @duration
+
+  _start: ->
+
+    @_timer = null
+    if @duration is 0
+      @_onUpdate @computeValueAtProgress 1
+      @finish()
+      return
+
+    @startTime = Date.now()
+    @_requestAnimationFrame()
+
+type.overrideMethods
+
+  __computeValue: ->
+
+    @_lastTime = @time
+    @_lastValue = @value
+    @_velocity.reset()
+
+    @time = Math.min @duration, Date.now() - @startTime
+    @progress = @computeProgressAtTime @time
+    return @value = @computeValueAtProgress @progress
+
+  __didStart: ->
+    return @_start() if @delay <= 0
+    @_timer = Timer @delay, => @_start()
+
+  __didUpdate: (value) ->
+    @finish() if @time is @duration
+
+  __didEnd: ->
+    return unless @_timer
+    @_timer.prevent()
+    @_timer = null
+
+  __captureFrame: ->
+    { @progress, @value, @time }
+
+module.exports = type.build()
